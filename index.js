0bd28783f60e9e714caab82d6ce17b219fc80eaf
Jared Thomas <jcjt929@web-development-teaching.com>
2022-04-06 00:45:45 -0400

    adding the work for the home page

---
+++ index.js
@@ -0,0 +1,35 @@
+document.querySelector("#course-grid-sidebar-link").addEventListener('mouseover', function (event) {
+  document.querySelector("#course-grid-sidebar-content").style.display = "block"
+}, false);
+
+document.querySelector("#course-grid-sidebar-content").addEventListener('mouseover', function (event) {
+  document.querySelector("#course-grid-sidebar-content").style.display = "block"
+}, false);
+
+document.querySelector("#course-grid-sidebar-link").addEventListener('mouseleave', function (event) {
+  document.querySelector("#course-grid-sidebar-content").style.display = "none"
+}, false);
+
+document.querySelector("#course-grid-sidebar-content").addEventListener('mouseleave', function (event) {
+  document.querySelector("#course-grid-sidebar-content").style.display = "none"
+}, false);
+
+document.querySelector("#course-filter-sidebar-link").addEventListener('mouseover', function (event) {
+  document.querySelector("#course-filter-sidebar-content").style.display = "block"
+}, false);
+
+document.querySelector("#course-filter-sidebar-content").addEventListener('mouseover', function (event) {
+  document.querySelector("#course-filter-sidebar-content").style.display = "block"
+}, false);
+
+document.querySelector("#course-filter-sidebar-link").addEventListener('mouseleave', function (event) {
+  document.querySelector("#course-filter-sidebar-content").style.display = "none"
+}, false);
+
+document.querySelector("#course-filter-sidebar-content").addEventListener('mouseleave', function (event) {
+  document.querySelector("#course-filter-sidebar-content").style.display = "none"
+}, false);
+
+document.querySelector("#course-filter-sidebar-content").addEventListener('mouseleave', function (event) {
+  document.querySelector("#course-filter-sidebar-content").style.display = "none"
+}, false);
